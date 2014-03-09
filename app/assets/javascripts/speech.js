387904f574453a67903d08993707f325235ba878
melhuang <melissahuang10@gmail.com>
2014-03-08 21:19:26 -0800

    input text

--- app/assets/javascripts/speech.js
+++ app/assets/javascripts/speech.js
@@ -1,27 +1,36 @@
-if (!('webkitSpeechRecognition' in window)) {
-    upgrade();
-} else{
-    var recognition - new webkitSpeechRecognition();
-    recognition.continuous = true; // speech pause stops recording
-    recognition.interimResults = true; //access to interim text
-    recognition.lang = "en";
+$(document).ready(function() {
+    if (!('webkitSpeechRecognition' in window)) {
+        upgrade();
+    } else {
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true; // speech pause stops recording
+        recognition.interimResults = true; //access to interim text
+        recognition.lang = "en";
 
-    var input = document.getElementById('textbox');
+        var textbox = $('#test');
 
-    recognition.onstart=function(event) {
-        var pos = textArea.getCursorPosition() - interimResult.length;
-        var final_transcript = '';
         recognition.start();
+
+        recognition.onstart=function(event) {
+            console.log("on start");
+            console.log(event);
+
+            //var pos = textArea.getCursorPosition() - interimResult.length;
+            }
+        /* the input */
+        recognition.onresult = function (event) {
+            var results = event.results;
+            console.log(results);
+            for (var i=0; i < results.length; i++) {
+                var word = results[i][0].transcript;
+                console.log(results[i][0].transcript);
+                textbox.val(word);
+            }
+        }
         
 
+
+
+        recognition.stop();
     }
-    /* the input */
-    var main=startInput();
-    recognition.onresult = function (event) {
-
-    }
-    
-
-
-
-    recognition.stop();
+});
