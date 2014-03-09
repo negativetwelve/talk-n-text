c962de40a48f38dda2a0aea46357798d47538add
melhuang <melissahuang10@gmail.com>
2014-03-08 23:59:03 -0800

    fix me

--- app/assets/javascripts/speech.js
+++ app/assets/javascripts/speech.js
@@ -15,29 +15,40 @@ $(document).ready(function() {
             console.log("on start");
 
             //var pos = textArea.getCursorPosition() - interimResult.length;
-            }
+        }
         /* the input */
-        
         recognition.onresult = function (event) {
+            console.log("entered result");
             var results = event.results;
             var final_tran = "";
             var interim_tran = "";
+            //var define_func = ["define", "a", "function"];
+            var define_func = "define a function";
+            var args = ["that", "takes", "in"]; 
+            var define = false;
             for (var i=event.resultIndex; i < results.length; i++) {
-                if (event.results[i].isFinal) {
-                    final_tran += event.results[i][0].transcript;
-                } else {
-                    interim_tran += event.results[i][0].transcript;
-                }
-                var word = results[i][0].transcript;
+                var curr_word_obj = event.results[i];
+                if (curr_word_obj.isFinal) {
+                    var curr_word = curr_word_obj[0].transcript;
+                
 
-                if (word == "hello") {
-                    word = "hi";
+                    console.log("curr_word: " + curr_word + "\n");
+
+                    if (curr_word == define_func) {
+                        define = true;
+                        editor.insert("def ");
+                        console.log("curr_word(in def): " + curr_word);
+                    }
+                    if (define) { 
+                        
+                    }
+
+                    //final_tran += curr_word; 
                 }
-                console.log(word);
-                editor.insert(final_tran);
+
+                    
             }
-        }
-        
+        } 
 
 
 
