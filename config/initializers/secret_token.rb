1ed58562254239a514955e5ecd252fd83600cf29
Mark Miyashita <negativetwelve@gmail.com>
2014-03-08 17:43:29 -0800

    Add base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TalkNText::Application.config.secret_key_base = '789258e4e0c6e60356d97fa12f25e8a209579e3f12af0dead9aa659e3f08fcd11aacb3a898f21a8c5f4cc227af532fc4dc2120273b329008898302cfd9486b2f'
